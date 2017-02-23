afbb5e3f53582f4b35b34c4fa21dc781221f5296
Matteo <mattebolo98@gmail.com>
2017-02-23 16:02:09 +0100

    Un sacco di stampe

--- stampe.py
+++ stampe.py
@@ -14,4 +14,29 @@ print 3 > 4
 
 print 3 < 10
 
+name = "Matteo"
+age = 18
+delta = 10
+template = "Ciao, mi chiamo %s e ho %s anni" 
+
+sentence1 = "Ciao mamma"
+sentence2 = "guarda come mi diverto" 
+
+
+
+print "Ciao, mi chiamo %s e ho %s anni" % (name, age)
+print "Ciao, sono %s" % name
+
+print "Ciao, mi chiamo %s e %s anni fa avevo %s anni" % (name, delta, age - delta)
+
+print template % (name, age)
+
+print sentence1 + " " + sentence2
+
+print """ 
+Lorem ipsum
+dolor sit amet
+"""
+
+print "Ciao, \n mi chiamo Matteo \n\n e ho 18 anni."
 
